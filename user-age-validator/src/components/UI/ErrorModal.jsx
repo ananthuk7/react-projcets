68c7cad13cb3c95296ec20cc141c96e5f14ca83e
Ananthu Krishnan <ananthukrish.k@gmail.com>
2023-09-21 20:16:28 +0530

    portal added to the user validation project

--- user-age-validator/src/components/UI/ErrorModal.jsx
+++ user-age-validator/src/components/UI/ErrorModal.jsx
@@ -1,22 +1,36 @@
-
+import ReactDom from 'react-dom'
 import Button from './Button'
 import PropTypes from 'prop-types'
 import classes from '../UI/ErrorModal.module.css'
 import Card from './Card'
 
+const BackDrop = ({ children, handleClose }) => {
+    return <>
+        <div className={classes.backdrop} onClick={handleClose}>{children}</div>
+    </>
+}
+
+const Modal = ({ error, handleClose }) => {
+    return <>
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{error.title}</h2>
+            </header>
+            <div className={classes.content}>{error.message}</div>
+            <footer className={classes.actions}>
+                <Button onClick={handleClose}> Okay</Button>
+            </footer>
+        </Card>
+    </>
+}
+
 const ErrorModal = ({ error, handleClose }) => {
     return (
-        <div className={classes.backdrop}>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{error.title}</h2>
-                </header>
-                <div className={classes.content}>{error.message}</div>
-                <footer className={classes.actions}>
-                    <Button onClick={handleClose}> Okay</Button>
-                </footer>
-            </Card>
-        </div >
+        <>
+            {ReactDom.createPortal(<BackDrop handleClose={handleClose} />, document.getElementById('backdrop'),)}
+            {ReactDom.createPortal(<Modal error={error} handleClose={handleClose} />, document.getElementById('modal'),)}
+
+        </>
     )
 }
 
@@ -24,6 +38,20 @@ const ErrorModal = ({ error, handleClose }) => {
 ErrorModal.propTypes = {
     error: PropTypes.object,
     handleClose: PropTypes.func
+}
+
+BackDrop.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.arrayOf(PropTypes.node)
+    ]),
+    handleClose: PropTypes.func
+}
+
+Modal.propTypes = {
+    handleClose: PropTypes.func,
+    error: PropTypes.object,
+
 }
 
 export default ErrorModal
